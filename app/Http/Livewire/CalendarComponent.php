82d03562f11f1aa18961ccb4f05e431f79f09d03
xon-g <xon0502@gmail.com>
2023-07-12 13:14:13 +0800

    Users now able to book appointment with therapist (with validation to avoid overlapping appointments)

--- app/Http/Livewire/CalendarComponent.php
+++ app/Http/Livewire/CalendarComponent.php
@@ -2,15 +2,88 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Appointment;
+use DebugBar\DataCollector\MessagesCollector;
+use DebugBar\DebugBar;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class CalendarComponent extends Component
 {
     public $therapist;
+    // public $startTime;
+    // public $endTime;
 
     public function mount($therapist)
     {
         $this->therapist = $therapist;
+    }
+
+    public function bookNow($startTime, $endTime)
+    {   
+        $debugbar = new DebugBar();
+        $debugbar->addCollector(new MessagesCollector());
+        $debugbar['messages']->info('hello world');
+
+        $startTime = Carbon::parse($startTime);
+        $endTime = Carbon::parse($endTime);
+
+        // Check for overlapping appointments with therapist
+        $buffer = 1;
+        $therapistAppointments = Appointment::where('therapist_id', $this->therapist->id)
+        ->where(function ($query) use ($startTime, $endTime) {
+            $query
+            ->where(function ($query) use ($startTime, $endTime) {
+                $query
+                ->where('appointment_start_date', '>=', $startTime)
+                ->where('appointment_start_date', '<', $endTime);
+            })
+            ->orWhere(function ($query) use ($startTime, $endTime) {
+                $query
+                ->where('appointment_end_date', '>', $startTime)
+                ->where('appointment_end_date', '<=', $endTime);
+            });
+        })
+        ->exists();
+
+        if ($therapistAppointments) {
+            // There is an overlapping appointment with the therapist
+            return redirect()->back()->with('error', 'There is an overlapping appointment with the therapist. Please reschedule.');
+        }
+
+        // Check for overlapping appointments with the user
+        $userAppointments = Appointment::where('user_id', Auth::user()->id)
+        ->where(function ($query) use ($startTime, $endTime) {
+            $query
+            ->where(function ($query) use ($startTime, $endTime) {
+                $query
+                ->where('appointment_start_date', '>=', $startTime)
+                ->where('appointment_start_date', '<', $endTime);
+            })
+            ->orWhere(function ($query) use ($startTime, $endTime) {
+                $query
+                ->where('appointment_end_date', '>', $startTime)
+                ->where('appointment_end_date', '<=', $endTime);
+            });
+        })
+        ->exists();
+
+        if ($userAppointments) {
+            // There is an overlapping appointment with the user
+            return redirect()->back()->with('error', 'You have an overlapping appointment. Please reschedule.');
+        } 
+        
+        // Proceed to create a new appointment
+        $appointment = new Appointment();
+        $appointment->user_id = Auth::user()->id;
+        $appointment->therapist_id = $this->therapist->id;
+        $appointment->appointment_start_date = $startTime;
+        $appointment->appointment_end_date = $endTime;
+        $appointment->save();
+        
+        // Success message or redirection
+        return redirect()->back()->with('success', 'Appointment created successfully.');
     }
 
     public function render()
--- resources/views/livewire/calendar-component.blade.php
+++ resources/views/livewire/calendar-component.blade.php
@@ -1,33 +1,50 @@
 <div>
-    <div id="calendar">
-        {{-- The whole world belongs to you. --}}
-    </div>
-
-    <div class="flex flex-column pt-5">
-        <div class="w-1/2 flex flex-column">
-            <div id="startTime">00:00</div>
-            <div>&nbsp; -- &nbsp;</div>
-            <div id="endTime">00:00</div>
+    @if (session('success'))
+        <div class="p-5 mb-5 bg-[#55CC80]">
+            {{ session('success') }}
         </div>
-    
-        <div class="w-1/2 flex justify-end">
-            <div class="cursor-pointer bg-blue-500 text-white py-2 px-4 rounded-lg btn btn-primary">
-                Book Now
+    @else
+        @if (session('error'))
+            <div class="p-5 mb-5 bg-[#FA5661]" >
+                {{ session('error') }}
+            </div>
+        @endif
+        <div id="calendar">
+            {{-- The whole world belongs to you. --}}
+        </div>
+        <div class="flex flex-column pt-5">
+            <div class="w-1/2 flex flex-column">
+                <div id="startTime">00:00</div>
+                <div>&nbsp; to &nbsp;</div>
+                <div id="endTime">00:00</div>
+            </div>
+        
+            <div class="w-1/2 flex justify-end">
+                <div onclick="clickBook()" class="cursor-pointer bg-blue-500 text-white py-2 px-4 rounded-lg btn btn-primary">
+                    Book Now
+                </div>
             </div>
         </div>
-    </div>
+    @endif
+        
     
     <script>
-        document.addEventListener('livewire:load', function () {
+        var selectedStartTimeValue;
+        var selectedEndTimeValue;
+
+        function renderCalendar(args) {
+            var {initialView} = args;
+
             var Calendar = window.Calendar;
             var dayGridPlugin = window.dayGridPlugin;
             var timeGridPlugin = window.timeGridPlugin;
             var interactionPlugin = window.interactionPlugin;
-
+    
             var calendarEl = document.getElementById('calendar');
             window.calendar = new Calendar(calendarEl, {
                 // Configure your calendar options here
                 plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+                initialView: initialView,
                 headerToolbar: {
                     start:'prev next today',
                     center: 'title',
@@ -45,28 +62,31 @@
                     if (selectionInfo.view.type==="dayGridMonth") {
                         calendar.changeView('timeGridWeek', selectionInfo.startStr);
                     } else {
-                        console.log(selectionInfo)
-                        setMinimumDuration(selectionInfo, 60);
-                        var start = new Date(selectionInfo.startStr);
-                        var end = new Date(selectionInfo.endStr)
+                        selectedStartTimeValue = selectionInfo.startStr;
+                        selectedEndTimeValue = setMinimumDuration(selectionInfo, 60);
+    
+                        var start = new Date(selectedStartTimeValue);
+                        var end = new Date(selectedEndTimeValue)
+    
                         var startTime = document.getElementById('startTime');
                         var endTime = document.getElementById('endTime');
                         startTime.innerHTML = start.toLocaleString('en-US', {hour:'numeric', minute:'numeric'});
                         endTime.innerHTML = end.toLocaleString('en-US', {hour:'numeric', minute:'numeric'});
-
-                    }
-                    
+                    }   
                 }
-
             });
             window.calendar.render();
+        }
+
+        document.addEventListener('livewire:load', function () {
+            renderCalendar({initailView: 'dayGridMonth'})
         });
 
 
         function setMinimumDuration(info, minutes) {
             var start = new Date(info.startStr);
-            var end = new Date(info.endStr);
-    
+            var end = new Date(info.endStr);    
+
             // Calculate the duration in minutes
             var duration = Math.abs((end - start) / (1000 * minutes));
 
@@ -75,8 +95,37 @@
                 end = new Date(start.getTime() + 60 * 60 * 1000);
                 window.calendar.select(start, end); // Update the selection
                 alert('Please select a minimum duration of 1 hour.');
-            }
+
+                var endFormat = new Date(end.toLocaleString());
+                endFormat.setMinutes(endFormat.getMinutes() - endFormat.getTimezoneOffset());
+                return endFormat.toISOString().substring(0, 19)+getTimezone()
+            } else {return info.endStr}
         }
 
+        function getTimezone() {
+            var date = new Date(); // Current date and time
+            var timezoneOffset = -date.getTimezoneOffset();
+
+            // Convert the offset to hours and minutes
+            var hours = Math.floor(Math.abs(timezoneOffset) / 60);
+            var minutes = Math.abs(timezoneOffset) % 60;
+
+            // Format the timezone string
+            var timezone = (timezoneOffset >= 0 ? '+' : '-') + hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
+            return timezone;
+        }
+
+        async function clickBook() {
+            if (!selectedStartTimeValue || !selectedEndTimeValue) {
+                alert('Please select a minimum duration of 1 hour.');
+            } else {
+                await @this.bookNow(selectedStartTimeValue, selectedEndTimeValue);
+                await renderCalendar({initialView: 'timeGridWeek'});
+                await window.calendar.select({
+                    start: selectedStartTimeValue,
+                    end: selectedEndTimeValue,
+                })
+            };
+        }
     </script>
 </div>
